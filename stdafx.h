293047eadf89ba168c39903461300038f2251128
RaflereaK <raflereak@gmail.com>
2022-09-19 21:47:02 +0900

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <random>
+#include <fstream>
+#include <stdlib.h>
+#include <Windows.h>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
